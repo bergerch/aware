d5cdf8891d451bc502a89b8689e5c413e7e1b0d1
Christian Berger <cb@sec.uni-passau.de>
2019-03-21 11:37:33 +0100

    some changes in Latency CLient/Server

--- src/bftsmart/demo/microbenchmarks/ThroughputLatencyClient.java
+++ src/bftsmart/demo/microbenchmarks/ThroughputLatencyClient.java
@@ -15,11 +15,15 @@ limitations under the License.
 */
 package bftsmart.demo.microbenchmarks;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import bftsmart.tom.ServiceProxy;
 import bftsmart.tom.util.Storage;
 import bftsmart.tom.util.TOMUtil;
+
+import java.io.Writer;
 import java.nio.ByteBuffer;
 import java.security.InvalidKeyException;
 import java.security.KeyFactory;
@@ -194,7 +198,8 @@ public class ThroughputLatencyClient {
         }
 
         public void run() {
-            
+
+
             System.out.println("Warm up...");
 
             int req = 0;
@@ -233,7 +238,7 @@ public class ThroughputLatencyClient {
                 else
                         proxy.invokeOrdered(request);
                         
-                if (verbose) System.out.println(this.id + " // sent!");
+                if (verbose) System.out.println(numberOfOps + " // sent!");
                 st.store(System.nanoTime() - last_send_instant);
 
                 if (interval > 0) {
@@ -247,12 +252,60 @@ public class ThroughputLatencyClient {
                 if (verbose && (req % 1000 == 0)) System.out.println(this.id + " // " + req + " operations sent!");
             }
 
+
+
             if(id == initId) {
+                String line = "0  , BFT-SMart , ";
                 System.out.println(this.id + " // Average time for " + numberOfOps / 2 + " executions (-10%) = " + st.getAverage(true) / 1000 + " us ");
+                line += st.getAverage(true) / 1000 + ",";
                 System.out.println(this.id + " // Standard desviation for " + numberOfOps / 2 + " executions (-10%) = " + st.getDP(true) / 1000 + " us ");
+                line += st.getDP(true) / 1000 + ",";
                 System.out.println(this.id + " // Average time for " + numberOfOps / 2 + " executions (all samples) = " + st.getAverage(false) / 1000 + " us ");
                 System.out.println(this.id + " // Standard desviation for " + numberOfOps / 2 + " executions (all samples) = " + st.getDP(false) / 1000 + " us ");
                 System.out.println(this.id + " // Maximum time for " + numberOfOps / 2 + " executions (all samples) = " + st.getMax(false) / 1000 + " us ");
+                System.out.println(this.id + " // 50th percentile latencies " + numberOfOps / 2 + " executions (all samples) = " + st.getPercentile(0.5) / 1000 + " us ");
+                line +=  st.getPercentile(0.5) / 1000 + ",";
+                System.out.println(this.id + " // 90th percentile latencies " + numberOfOps / 2 + " executions (all samples) = " + st.getPercentile(0.9) / 1000 + " us ");
+                line +=  st.getPercentile(0.9) / 1000 + ",";
+                line += "\n";
+
+                Writer output;
+                try {
+                    output = new BufferedWriter(new FileWriter("measurement-client" + id, true));
+                    output.append(line);
+                    output.close();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            System.out.println("Fnished! ... but will continue sending requests...");
+            for (int i = 0; i < numberOfOps-1 ; i++, req++) {
+
+                if(readOnly)
+                    proxy.invokeUnordered(request);
+                else
+                    proxy.invokeOrdered(request);
+
+
+                if (verbose && (req % 100 == 0)) System.out.println(this.id + " // " + req + " operations sent!");
+
+                if (interval > 0) {
+                    try {
+                        //sleeps interval ms before sending next request
+                        Thread.sleep(interval);
+                    } catch (InterruptedException ex) {
+                    }
+                }
+                if (interval < 0) { // if interval negative, use it as upper limit for a randomized interval
+                    try {    // so wait between 0ms and interval ms
+                        double waitTime = Math.random() * interval * -1;
+                        System.out.println("waiting for " + waitTime + " ms");
+                        Thread.sleep(Math.round(waitTime));
+                    } catch (InterruptedException ex) {
+                    }
+                }
             }
             
             proxy.close();
